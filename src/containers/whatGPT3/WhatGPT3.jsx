f9f5c5629ce9e9ccb5be01768e5de6bf2da7be79
johnson419 <johnsonfredrick577@.com>
2023-03-28 02:22:11 +0300

    Added the whatgpt section to the project

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import './whatGPT3.css'
+import React from 'react';
+import Feature from '../features/Features';
+import './whatGPT3.css';
 
 const whatGPT3 = () => {
   return (
-    <div>whatGPT3</div>
-  )
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature title='What is GPT-3' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'/>          
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <div className='gpt3__whatgpt3-container__feature'>
+        <Feature title='Chatbots' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought.'/>
+        </div>
+        <div className='gpt3__whatgpt3-container__feature'>
+        <Feature title='Knowledgebase' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments'/>
+        </div>
+        <div className='gpt3__whatgpt3-container__feature'>
+        <Feature title='Education' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments'/>
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default whatGPT3
