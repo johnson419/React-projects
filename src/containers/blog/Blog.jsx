fea2546016b9d4378915b3aa44599dcebee11b2a
johnson419 <johnsonfredrick577@.com>
2023-04-13 13:25:24 +0300

    added the footer and article section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './blog.css'
+import {blog01, blog02, blog03, blog04, blog05} from './imports'
+import { Article } from '../../components'
 
 const Blog = () => {
   return (
@@ -8,8 +10,15 @@ const Blog = () => {
         <h1 className='gradient__text'>Alot is happening, We are blogging about it.</h1>
       </div>
       <div className='gpt3__blog-container'>
-        <div className='gpt3__blog-container_groupA'></div>
-        <div className='gpt3__blog-container_groupB'></div>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgUrl={blog01} />
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+        <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article  imgUrl={blog03}date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog04} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+        <Article imgUrl={blog05} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+        </div>
       </div>
     </div>
   )
